e8dd13634ee69888ae9dbbe11ae4e11a9e064f93
shazly333 <shazly0120100@gmail.com>
2017-09-30 18:25:00 +0200

    Complement

--- main.cpp
+++ main.cpp
@@ -44,75 +44,111 @@ void Union()
 }
 void Intersection()
 {
- cout<<"Enter The Id Of The sets\n";
-            int A,B;
-            cin>>A>>B;
-            while(A>no||B>no)
-            {
-                cout<<"Enter The Id Of The sets\n";
-                int A,B;
-                cin>>A>>B;
+    cout<<"Enter The Id Of The sets\n";
+    int A,B;
+    cin>>A>>B;
 
-            }
-            string output[10000];
-            int u=0;
-            for(int i=0; i<sett[B].size(); i++)
+    string output[10000];
+    int u=0;
+    for(int i=0; i<sett[B].size(); i++)
+    {
+        string word=sett[B][i];
+        for(int j=0; j<sett[A].size(); j++)
+        {
+            if(sett[A][j]==word)
             {
-                string word=sett[B][i];
-                for(int j=0; j<sett[A].size(); j++)
-                {
-                    if(sett[A][j]==word)
-                    {
-                        output[u]=word;
-                        u++;
-                        break;
-                    }
-                }
+                output[u]=word;
+                u++;
+                break;
             }
-            for(int i=0; i<u; i++)
-            {
-                cout<<output[i]<<" ";
+        }
+    }
+    for(int i=0; i<u; i++)
+    {
+        cout<<output[i]<<" ";
 
-            }
-            cout<<"\n";
+    }
+    cout<<"\n";
 
 }
 void Difference()
 {
-     cout<<"Enter The Id Of The sets\n";
-            int A,B;
-            cin>>A>>B;
-            pair< string,bool> output[10000];
-            int u=sett[A].size();
-            for(int i=0; i<sett[A].size(); i++)
-                output[i]=make_pair(sett[A][i],1);
-            for(int i=0; i<sett[B].size(); i++)
+    cout<<"Enter The Id Of The sets\n";
+    int A,B;
+    cin>>A>>B;
+    pair< string,bool> output[10000];
+    int u=sett[A].size();
+    for(int i=0; i<sett[A].size(); i++)
+        output[i]=make_pair(sett[A][i],1);
+    for(int i=0; i<sett[B].size(); i++)
+    {
+        string word=sett[B][i];
+        int flag=0;
+        for(int j=0; j<u; j++)
+        {
+            if(output[j].first==word)
             {
-                string word=sett[B][i];
-                int flag=0;
-                for(int j=0; j<u; j++)
-                {
-                    if(output[j].first==word)
-                    {
-                        output[i]=make_pair(word,0);
-                        flag=1;
-                        break;
-                    }
-                }
-                if(!flag)
-                {
-                    output[u]=make_pair(word,1);
-                    u++;
-                }
+                output[i]=make_pair(word,0);
+                flag=1;
+                break;
+            }
+        }
+        if(!flag)
+        {
+            output[u]=make_pair(word,1);
+            u++;
+        }
+
+    }
+
+    for(int i=0; i<u; i++)
+    {
+        if(output[i].second)
+            cout<<output[i].first<<" ";
+    }
+    cout<<"\n";
+
+}
+void complement()
+{
+
+    cout<<"Enter The Id Of The set\n";
+    int A;
+    cin>>A;
+    string output[10000];
+    int u=sett[A].size();
+    for(int i=0; i<sett[A].size(); i++)
+        output[i]=sett[A][i];
+   /* for(int i=0; i<sett[B].size(); i++)
+    {
+        string word=sett[B][i];
+        int flag=0;
+        for(int j=0; j<u; j++)
+        {
+            if(output[j]==word)
+            {
+                flag=1;
+                break;
 
             }
+        }
 
-            for(int i=0; i<u; i++)
-            {
-                if(output[i].second)
-                    cout<<output[i].first<<" ";
-            }
-            cout<<"\n";
+        if(!flag)
+        {
+
+            output[u]=word;
+
+            u++;
+        }
+    }
+    for(int i=0; i<u; i++)
+    {
+        cout<<output[i]<<" ";
+    }
+    cout<<"\n";
+
+*/
+
 
 }
 int main()
@@ -123,7 +159,7 @@ int main()
 
     cout<<"Please Enter The Universe\n";
     for(int i=0; i<x; i++)
-    cin>>Universe[i];
+        cin>>Universe[i];
     cout<<"Please Enter The Number Of Sets\n";
     cin>>no;
 
@@ -143,7 +179,7 @@ int main()
     }
     while(1)
     {
-        cout<<"1-Union\n2-Intersection\n3-Difference\nEnter Your Choice\n";
+        cout<<"1-Union\n2-Intersection\n3-Difference\n4-Complement\nEnter Your Choice\n";
         int choice;
         cin>>choice;
         if(choice==1)
@@ -156,7 +192,11 @@ int main()
         }
         else if(choice==3)
         {
-           Difference();
+            Difference();
+        }
+        else if(choice==4)
+        {
+            complement();
         }
 
     }
